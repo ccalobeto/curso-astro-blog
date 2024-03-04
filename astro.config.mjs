1ceb75d1a76ccb64636dd0a2d0d63778d5ff829a
ccalobeto <calobeto.leon@gmail.com>
2024-03-04 10:55:23 -0500

    config redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,4 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  redirects: {
-    '/internet': 'https://ccalobeto.github.io/cobertura_internet/',
-    'hi': 'blog'
-  }
 });
